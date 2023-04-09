72e9aab211c60ad2b41f9637a0b7ffbcd209e757
Yağız Erkam "Plawlost" Çelebi <84669529+plawlost@users.noreply.github.com>
2023-04-09 23:16:34 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -13,21 +13,25 @@ document.addEventListener("mousemove", updateCoordinates);
 
 // When the extension is opened, set the focus on the coordinates container to enable scrolling
 window.addEventListener("load", function() {
-  coordinatesContainer?.focus();
+  if (coordinatesContainer) {
+    coordinatesContainer.focus();
+  }
 });
 
 // Add a click event listener to the coordinates container to copy the coordinates to the clipboard
-coordinatesContainer?.addEventListener("click", function() {
-  // Create a temporary input element to copy the coordinates to the clipboard
-  const input = document.createElement("input");
-  input.setAttribute("value", coordinates.textContent);
-  document.body.appendChild(input);
-  input.select();
+if (coordinatesContainer) {
+  coordinatesContainer.addEventListener("click", function() {
+    // Create a temporary input element to copy the coordinates to the clipboard
+    const input = document.createElement("input");
+    input.setAttribute("value", coordinates.textContent);
+    document.body.appendChild(input);
+    input.select();
 
-  // Copy the coordinates to the clipboard and remove the temporary input element
-  document.execCommand("copy");
-  document.body.removeChild(input);
-});
+    // Copy the coordinates to the clipboard and remove the temporary input element
+    document.execCommand("copy");
+    document.body.removeChild(input);
+  });
+}
 
 // When the popup is closed, remove the event listener for updating the coordinates to avoid performance issues
 window.addEventListener("unload", function() {
